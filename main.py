2b0b388fa92adbc223c5481bfcba7c1b63a10660
TANK-MARK-V <dvornikov.mark@mail.ru>
2025-04-17 18:43:58 +0700

    Роутеры удобно распределены

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ from aiogram.client.default import DefaultBotProperties
 
 import config
 from handlers import router
-from lolgen import router_lolgen
-from STI import router_sti
-from EVO import router_evo
-from admin import router_admin
-from coding import router_coding
+from lolgen_bot import router_lolgen as lolgen
+from STI_bot import router_sti as sti
+from EVO_bot import router_evo as evo
+from admin import router_admin as admin
+from coding import router_coding as coding
+from irregular_verbs import router_verbs as verbs
+from ATOM import router_atom as atom
+from valent import router_val as val
+from free_handler import free_router as free
+
+ROUTERS = [router, lolgen, admin, coding, val, sti, evo, verbs, atom, free]
 
 
 async def main():
     print("Старт\n")
     bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
-    dp.include_routers(router_lolgen, router_sti, router_evo, router_admin, router_coding, router)
+    dp.include_routers(*ROUTERS)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-
 
 
 if __name__ == "__main__":
